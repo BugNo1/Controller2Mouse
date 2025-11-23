c467712f4c0a252652c20a397e40a2b7f1a262a0
BugNo1 <128540046+BugNo1@users.noreply.github.com>
2025-11-23 12:07:34 +0100

    small refactoring

--- main.py
+++ main.py
@@ -2,10 +2,83 @@ import time
 import pygame
 from pynput.mouse import Controller, Button
 
+JOYSTICK_1 = 0
+JOYSTICK_2 = 1
+
 x_offset_max = 100
 y_offset_max = 100
 x_offset = 0
 y_offset = 0
+
+def handle_button_down_event(event):
+    # print(event.button)
+    if event.button == 0:  # cross button
+        if event.instance_id == JOYSTICK_1:
+            mouse.press(Button.left)
+    if event.button == 1:  # circle button
+        pass
+    if event.button == 2:  # square button
+        pass
+    if event.button == 3:  # triangle button
+        if event.instance_id == JOYSTICK_1:
+            mouse.press(Button.right)
+    if event.button == 6:  # options button
+        pass
+    if event.button == 11:  # up button
+        pass
+    if event.button == 12:  # down button
+        pass
+    if event.button == 13:  # left button
+        pass
+    if event.button == 14:  # right button
+        pass
+
+def handle_button_up_event(event):
+    if event.button == 0:  # cross button
+        if event.instance_id == JOYSTICK_1:
+            mouse.release(Button.left)
+    if event.button == 1:  # circle button
+        pass
+    if event.button == 2:  # square button
+        pass
+    if event.button == 3:  # triangle button
+        if event.instance_id == JOYSTICK_1:
+            mouse.release(Button.right)
+    if event.button == 6:  # options button
+        pass
+    if event.button == 11:  # up button
+        pass
+    if event.button == 12:  # down button
+        pass
+    if event.button == 13:  # left button
+        pass
+    if event.button == 14:  # right button
+        pass
+
+def handle_axis_motion_event(event):
+    joystick = joysticks[event.instance_id]
+    # print(event.instance_id)
+    x = joystick.get_axis(0)
+    y = joystick.get_axis(1)
+    if y <= -0.1:
+        # print("up: %0.2f" % y)
+        y_offset = y * y_offset_max
+    if y >= 0.1:
+        # print("down: %0.2f" % y)
+        y_offset = y * y_offset_max
+    if (y > -0.1) and (y < 0.1):
+        # print("y centered: %0.2f" % y)
+        y_offset = 0
+    if x <= -0.1:
+        # print("left: %0.2f" % x)
+        x_offset = x * x_offset_max
+    if x >= 0.1:
+        # print("right: %0.2f" % x)
+        x_offset = x * x_offset_max
+    if (x > -0.1) and (x < 0.1):
+        # print("x centered: %0.2f" % x)
+        x_offset = 0
+    mouse.move(int(x_offset), int(y_offset))
 
 pygame.init()
 pygame.joystick.init()
@@ -20,53 +93,18 @@ while not done:
     # PyGame seems to have an issue with event processing - But when Steam is running everything is fine!
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True  # Flag that we are done so we exit this loop.
+            done = True
 
         if event.type == pygame.JOYBUTTONDOWN:
-            #print("Joystick button pressed: %d" % event.button)
-            #print()
-            if event.button == 0:
-                #pyautogui.mouseDown(button='left')
-                mouse.press(Button.left)
-            if event.button == 2:
-                #pyautogui.mouseDown(button='right')
-                mouse.press(Button.right)
+            handle_button_down_event(event)
 
         if event.type == pygame.JOYBUTTONUP:
-            if event.button == 0:
-                #pyautogui.mouseUp(button='left')
-                mouse.release(Button.left)
-            if event.button == 2:
-                #pyautogui.mouseUp(button='right')
-                mouse.release(Button.right)
-            #print("Joystick button released: %d" % event.button)
-            #print()
+            handle_button_up_event(event)
 
         if event.type == pygame.JOYAXISMOTION:
-            joystick = joysticks[event.instance_id]
-            x = joystick.get_axis(0)
-            y = joystick.get_axis(1)
-            if y <= -0.1:
-                #print("up: %0.2f" % y)
-                y_offset = y * y_offset_max
-            if y >= 0.1:
-                #print("down: %0.2f" % y)
-                y_offset = y * y_offset_max
-            if (y > -0.1) and (y < 0.1):
-                #print("y centered: %0.2f" % y)
-                y_offset = 0
-            if x <= -0.1:
-                #print("left: %0.2f" % x)
-                x_offset = x * x_offset_max
-            if x >= 0.1:
-                #print("right: %0.2f" % x)
-                x_offset = x * x_offset_max
-            if (x > -0.1) and (x < 0.1):
-                #print("x centered: %0.2f" % x)
-                x_offset = 0
+            handle_axis_motion_event(event)
 
-    mouse.move(int(x_offset), int(y_offset))
-    time.sleep(0.05)
+    time.sleep(0.05)  # needed to not overload the event system
 
 
 # TODO:
@@ -75,3 +113,4 @@ while not done:
  # square = SPACE
  # right joystick = arrow keys (repeat)
  # cross keys on left side = arrow keys - but pressed = pressed and released = released (if possible)
+ # control = FN + F5
